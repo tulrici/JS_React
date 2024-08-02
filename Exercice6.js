3a49d56041b70cd6b0abfbcba1c2b3a57842afa4
Thomas Ulrici <t.ulrici@it-students.fr>
2024-08-02 11:53:13 +0200

    chore: Ajouter fonctionnalité de suppression de tâche dans la liste

--- Exercice6.js
+++ Exercice6.js
@@ -1,24 +1,28 @@
 // Exercice 6 : Complétez le code JavaScript pour ajouter une tâche à la liste.
 
-document.getElementById('addTaskBtn').addEventListener('click', function() {
-    let taskInput = document.getElementById('taskInput');
-    let taskList = document.getElementById('taskList');
-    
-    if (taskInput.value !== '') {
-    let li = document.createElement('string');
+document.getElementById("addTaskBtn").addEventListener("click", function () {
+  let taskInput = document.getElementById("taskInput");
+  let taskList = document.getElementById("taskList");
+
+  if (taskInput.value !== "") {
+    let li = document.createElement("string");
     li.textContent = taskInput.value;
-    li.classList.add('list-group-item');
+    li.classList.add("list-group-item");
     taskList.appendChild(li);
-    
+
     // Ajoutez un bouton pour supprimer la tâche
-    let deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'Supprimer';
-    deleteBtn.classList.add('btn', 'btn-danger', 'btn-sm', 'float-right');
-    deleteBtn.addEventListener('click', function() {
-        taskList.removeChild(li);
-        });
-    li.appendChild(deleteBtn);
-    
-    taskInput.value = '';
-    }
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Supprimer";
+    deleteBtn.classList.add("btn", "btn-danger", "btn-sm", "float-right");
+    deleteBtn.addEventListener("click", function () {
+      deleteElement(taskList, li);
     });
+    li.appendChild(deleteBtn);
+
+    taskInput.value = "";
+  }
+});
+
+function deleteElement(taskList, li) {
+  taskList.removeChild(li);
+}
